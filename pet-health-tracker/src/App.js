b833a906b54b584ffd5679a5b497b540ae3fa70d
M. <maricamolder@gmail.com>
2025-06-25 15:13:04 -0400

    Downgrade tailwind to v3

--- pet-health-tracker/src/App.js
+++ pet-health-tracker/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import AddPetForm from './AddPetForm';
+import './globals.css';
+import AddPetForm from './Components/AddPetForm';
 
 function App() {
   return (
-    <div className="App">
-      <div className="min-h-screen bg-gray-100">
+    <div className="min-h-full flex">
+      <div className="bg-gray-100 flex flex-row m-10">
         <header className="bg-white shadow-sm">
           <div className="max-w-7xl mx-auto px-4 py-6">
             <h1 className="text-3xl font-bold text-gray-900">
